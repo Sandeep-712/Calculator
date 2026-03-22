11809772361f079716619262ed8030eabc5af647
Sandy712 <sandeepprajapat200@gmail.com>
2026-03-22 20:06:48 +0530

    Successfully completed and working

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,42 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+    static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        InputReader input = new InputReader(scanner);
+        Calculator calc = new Calculator();
+        OutputWriter output = new OutputWriter();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+        try {
+            double a = input.readNumbers("Enter first numbers ");
+            String op = input.readOperator("Enter operator (+,-,*,/): ");
+            double b = input.readNumbers("Enter second number: ");
+
+            //Compute
+            double result = compute(calc, a, op, b);
+
+            output.printResult(a,op,b,result);
+        }catch (Exception e){
+            output.printError(e.getMessage());
+            System.exit(1);
+        }finally {
+            scanner.close();
         }
     }
+
+    private static double compute(Calculator calc, double a,String op,double b){
+        return switch (op) {
+            case "+" -> calc.add(a, b);
+            case "-" -> calc.subtract(a, b);
+            case "*" -> calc.multiply(a, b);
+            case "/" -> calc.divide(a, b);
+            default -> throw new IllegalArgumentException("Unknown operator: " + op);
+        };
+    }
 }
+
+
